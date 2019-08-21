2b0639728fc3f365e4f73662b7cf42bd2a9f3ca5
lihaifeng <haifengli@miliantech.com>
2019-08-21 15:32:52 +0800

    注册后直接登录

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ApplicationRecord
   before_save {self.email = email.downcase}  # 数据库中存储email均为小写
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 6}
   has_secure_password
+
+  # 返回指定字符串的哈希摘要
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
