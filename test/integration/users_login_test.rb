a005b7176d90daa57a1abfff9ebf13a932c3ab78
Uncle Maple <skipmaple@gmail.com>
2019-08-23 02:19:25 +0800

    添加　记住我　测试

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -39,14 +39,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_not_empty cookies['remember_token']
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
   end
 
   test "login without remembering" do
     # 登录， 设定cookie
     log_in_as(@user, remember_me: '1')
     delete logout_path
-    delete logout_path
+    assert_empty cookies['remember_token']
     # 再次登录， 确认 cookie 被删除了
     log_in_as(@user, remember_me: '0')
     assert_empty cookies['remember_token']
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,7 +25,7 @@ end
 
 class ActionDispatch::IntegrationTest
   # 登入指定的用户
-  def log_in_as(user, remember_me, password: 'password')
+  def log_in_as(user, password: 'password', remember_me: '1')
     post login_path, params: { session: { email: user.email,
                                           password: password,
                                           remember_me: remember_me } }
