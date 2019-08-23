b38ce6a9f04085b3191ec34626ddb10cf0b76832
lihaifeng <haifengli@miliantech.com>
2019-08-23 20:01:28 +0800

    编辑用户

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   # 返回指定用户的Gravatar
-  def gravatar_for(user, size: 80 )
+  def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://s.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
