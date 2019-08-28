10b25060884b66e2be1f9463f632adb31e81e72e
lihaifeng <haifengli@miliantech.com>
2019-08-28 11:39:48 +0800

    给用户邮箱发送链接激活

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title, "Iverfeng's little website"
-    assert_equal full_title("Help"), "Help | Iverfeng's little website"
+    assert_equal full_title, "Setaria"
+    assert_equal full_title("Help"), "Help | Setaria"
   end
 end
