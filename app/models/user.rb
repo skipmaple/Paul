0c5c8497ac718ff405e0ee572876fbeb5b50db5b
lihaifeng <haifengli@miliantech.com>
2019-09-02 20:13:45 +0800

    创建micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,15 @@ class User < ApplicationRecord
   before_save :downcase_email
   before_create :create_activation_digest
   # before_save {self.email = email.downcase}  # 数据库中存储email均为小写
-  validates :name, presence: true, length: {maximum: 50}
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255},
-                    format: {with: VALID_EMAIL_REGEX},
-                    uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_secure_password
+
+  has_many :microposts
 
   # 为了持久保存会话，在数据库中记住用户
   def remember
