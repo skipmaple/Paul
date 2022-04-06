1c10d9cff1a9c13e04df7232da5be3ba5be497a5
skipmaple <skipmaple@gmail.com>
2022-04-06 13:07:01 +0800

    fix: flowbite plugin import error.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,6 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "flowbite"
+import "flowbite/plugin"
 import "./dark_mode_switcher"
 
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -111,5 +111,6 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/typography'),
+        require('flowbite/plugin')
     ]
 }
