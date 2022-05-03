b5975c10f2a574fef208be610cb4a2e41c188422
drewlee <skipmaple@gmail.com>
2022-05-03 23:08:30 +0800

    feat: add user show page assets && paginate assets.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
+      @user = current_user
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
