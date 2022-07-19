8612c9ef3cf17528be6febe0c679eeed1d7a0a0e
drewlee <skipmaple@gmail.com>
2022-07-19 10:57:33 +0800

    feat: upgrade ruby version to 3.1.2 && upgrade rails version to 7.0.3.1 .

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,9 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about', as: 'about'
   get '/contact', to: 'static_pages#contact', as: 'contact'
   get '/signup', to: 'users#new'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  # get '/login', to: 'sessions#new'
+  # post '/login', to: 'sessions#create'
+  # delete '/logout', to: 'sessions#destroy'
 
   resources :users do
     member do
