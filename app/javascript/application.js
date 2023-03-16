39e272fc8eba894316d448e7462d2e25572cb1cd
drewlee <skipmaple@gmail.com>
2023-03-16 20:53:36 +0800

    fix: flash close btn.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,12 @@
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "flowbite"
+import '@hotwired/turbo-rails'
+import './controllers'
+import 'flowbite'
 import { getCookie, isExistThemeCookie } from './utils/cookies'
+
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+document.addEventListener('DOMContentLoaded', () => window.Alpine.start())
 
 // // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 // if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -11,9 +16,9 @@ import { getCookie, isExistThemeCookie } from './utils/cookies'
 // }
 
 if (getCookie('current_theme') === 'dark' || (!isExistThemeCookie && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark');
+  document.documentElement.classList.add('dark')
 } else {
-    document.documentElement.classList.remove('dark')
+  document.documentElement.classList.remove('dark')
 }
 
 
